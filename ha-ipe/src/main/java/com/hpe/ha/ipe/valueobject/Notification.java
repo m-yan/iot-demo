5f4a3d5002057c7a4988dbb0523542ac3ed5af11
masateru-okamoto <masateru.okamoto@hpe.com>
2017-08-29 15:15:14 +0900

    バグ修正

--- ha-ipe/src/main/java/com/hpe/ha/ipe/valueobject/Notification.java
+++ ha-ipe/src/main/java/com/hpe/ha/ipe/valueobject/Notification.java
@@ -1,18 +1,20 @@
 package com.hpe.ha.ipe.valueobject;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonRootName;
-
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @JsonRootName(value = "m2m:nev")
 @Data
 @NoArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class Notification {
 	private Representation rep;
 	
 	@Data
 	@NoArgsConstructor
+	@JsonIgnoreProperties(ignoreUnknown = true)
 	public static class Representation {
 		private String con;
 	}
--- ha-ipe/src/main/java/com/hpe/ha/ipe/web/ApiController.java
+++ ha-ipe/src/main/java/com/hpe/ha/ipe/web/ApiController.java
@@ -1,5 +1,8 @@
 package com.hpe.ha.ipe.web;
 
+import java.io.IOException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -7,13 +10,17 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
-
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.core.JsonParseException;
 import com.hpe.ha.ipe.domain.HomeStatus;
 import com.hpe.ha.ipe.service.HomeStatusService;
 import com.hpe.ha.ipe.service.IRemoconController;
 import com.hpe.ha.ipe.valueobject.HgwApi;
 import com.hpe.ha.ipe.valueobject.Notification;
-
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -21,6 +28,15 @@ import org.springframework.http.ResponseEntity;
 @RestController
 public class ApiController {
 
+	private static final Logger logger = LoggerFactory.getLogger(ApiController.class);
+
+	private static final ObjectMapper mapper = new ObjectMapper();	
+	static {
+		mapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
+		mapper.enable(SerializationFeature.WRAP_ROOT_VALUE);
+		mapper.enable(DeserializationFeature.UNWRAP_ROOT_VALUE);
+	}
+	
 	@Autowired
 	HomeStatusService homeStatusService;
 	
@@ -43,8 +59,22 @@ public class ApiController {
 	}
 
 	@PostMapping("/ha/ipe/forwardNotification")
-	public ResponseEntity<String> forwardNotification(@RequestBody Notification body, @RequestHeader("X-M2M-RI") String requestId) {
-		iRemoconCtl.sendInfrared(Integer.valueOf(body.getRep().getCon()));
+	public ResponseEntity<String> forwardNotification(@RequestBody String body, @RequestHeader("X-M2M-RI") String requestId) {
+		Notification notification = null;
+		  try {
+				notification = mapper.readValue(body, Notification.class);
+			} catch (JsonParseException e) {
+				logger.warn("Received JSON format is invalid.");
+				return null;
+			} catch (JsonMappingException e) {
+				logger.warn("Received Resource does not conform to the terms of oneM2M.");
+				logger.warn(e.getMessage());
+				return null;
+			} catch (IOException e) {
+				logger.error(e.getMessage(), e);
+				return null;
+			}
+		iRemoconCtl.sendInfrared(Integer.valueOf(notification.getRep().getCon()));
 		return ResponseEntity.ok().header("X-M2M-RI", requestId).header("X-M2M-RSC", "2000").body(null);
 	}
 	
