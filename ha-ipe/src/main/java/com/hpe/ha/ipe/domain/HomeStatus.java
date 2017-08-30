5eaff0c2479eb210ab0abf36f7e909dab5c6cb1e
masateru-okamoto <masateru.okamoto@hpe.com>
2017-08-30 11:30:38 +0900

    APIの仕様変更

--- ha-ipe/src/main/java/com/hpe/ha/ipe/domain/HomeStatus.java
+++ ha-ipe/src/main/java/com/hpe/ha/ipe/domain/HomeStatus.java
@@ -6,6 +6,7 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 import lombok.Data;
@@ -21,33 +22,35 @@ public class HomeStatus {
 	@Id
 	@NotNull
 	@NonNull
+	@JsonIgnore
 	private String id;
 	
 	@Column
-	private Integer monitoringMode = 1;
+	@NotNull
+	private boolean monitoringMode = false;
 	
-	@Column
-	private Integer motionDetectionStatus = 2;
-
-	@Column
-	private Float temperature = 25.0f;
-
-	@Column
-	private Integer humidity = 60;
-	
-	@Column
-	private Integer illuminance = 1000;
-	
-	@Column
-	private Boolean ocSensorPower = true;
-	
-	@Column
-	private Boolean mSensorPower = true;
-	
-	public void setEnvironmentalData(HomeStatus homeStatus) {
-		this.temperature = homeStatus.getTemperature();
-		this.humidity = homeStatus.getHumidity();
-		this.illuminance = homeStatus.getIlluminance();
-	}
-	
+//	@Column
+//	private Integer motionDetectionStatus = 2;
+//
+//	@Column
+//	private Float temperature = 25.0f;
+//
+//	@Column
+//	private Integer humidity = 60;
+//	
+//	@Column
+//	private Integer illuminance = 1000;
+//	
+//	@Column
+//	private Boolean ocSensorPower = true;
+//	
+//	@Column
+//	private Boolean mSensorPower = true;
+//	
+//	public void setEnvironmentalData(HomeStatus homeStatus) {
+//		this.temperature = homeStatus.getTemperature();
+//		this.humidity = homeStatus.getHumidity();
+//		this.illuminance = homeStatus.getIlluminance();
+//	}
+//	
 }
--- ha-ipe/src/main/java/com/hpe/ha/ipe/web/ApiController.java
+++ ha-ipe/src/main/java/com/hpe/ha/ipe/web/ApiController.java
@@ -8,6 +8,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -21,10 +22,8 @@ import com.fasterxml.jackson.core.JsonParseException;
 import com.hpe.ha.ipe.domain.HomeStatus;
 import com.hpe.ha.ipe.service.HomeStatusService;
 import com.hpe.ha.ipe.service.IRemoconController;
-import com.hpe.ha.ipe.valueobject.HgwApi;
 import com.hpe.ha.ipe.valueobject.Notification;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 
 @RestController
@@ -45,23 +44,21 @@ public class ApiController {
 	@Autowired
 	IRemoconController iRemoconCtl;
 		
-	@GetMapping(value= "/ems/devices/12345678/0101", produces = MediaType.APPLICATION_XML_VALUE)
-	public String getMonitoringMode() {
+	@GetMapping("/api/home_status")
+	HomeStatus getMonitoringMode() {
 		HomeStatus homeStatus = homeStatusService.findOne("12345678");
-		return String.format("<Device><Modules><hgwDataPoints><Data><monitoringMode>%d</monitoringMode></Data></hgwDataPoints></Modules></Device>", homeStatus.getMonitoringMode());
+		return homeStatus;
 	}
 	
-	@PostMapping("/ems/devices/12345678/0101")
+	@PutMapping("/api/home_status")
 	@ResponseStatus(HttpStatus.OK)
-	public void setMonitoringMode(@RequestBody HgwApi devices) {
-		int monitoringMode = devices.getModules().getHgwDataPoints().getData().getMonitoringMode();
-		HomeStatus homeStatus = homeStatusService.findOne("12345678");
-		homeStatus.setMonitoringMode(monitoringMode);
-		homeStatusService.update(homeStatus);
+	HomeStatus setMonitoringMode(@RequestBody HomeStatus status) {
+		status.setId("12345678");
+		return homeStatusService.update(status);
 	}
 
 	@PostMapping("/ha/ipe/forwardNotification")
-	public ResponseEntity<String> forwardNotification(@RequestBody String body, @RequestHeader("X-M2M-RI") String requestId) {
+	ResponseEntity<String> forwardNotification(@RequestBody String body, @RequestHeader("X-M2M-RI") String requestId) {
 		ContentInstance notifiedCin = null;
 		  try {
 				Notification notification = mapper.readValue(body, Notification.class);
