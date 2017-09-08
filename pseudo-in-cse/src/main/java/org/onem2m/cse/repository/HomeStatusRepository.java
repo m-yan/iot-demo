3587b4587ec8c692f60bb1c080a5e8940e6d7d30
masateru-okamoto <masateru.okamoto@hpe.com>
2017-09-08 13:30:02 +0900

    不要プロジェクト削除

--- pseudo-in-cse/src/main/java/org/onem2m/cse/repository/HomeStatusRepository.java
+++
@@ -1,8 +0,0 @@
-package org.onem2m.cse.repository;
-
-import org.onem2m.cse.domain.HomeStatus;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface HomeStatusRepository extends JpaRepository<HomeStatus, String>{
-
-}
